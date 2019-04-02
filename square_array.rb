8fdec1c00cd425979e859d7c17e25a45787c65bb
Jason  Montes <jasone.montes@gmail.com>
2019-04-02 01:35:05 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,11 @@
 def square_array(array)
-  
+   it 'does not call on collect/map or inject' do
+    numbers = [1,2,3]
+    expect(numbers).to_not receive(:collect)
+    expect(numbers).to_not receive(:map)
+    expect(numbers).to_not receive(:inject)
+    square_array(numbers)
+  end
   # your code here
 end
+
