b128dd948aeb164b4f3e42a9e18afdcf4a87e63e
Jason  Montes <jasone.montes@gmail.com>
2019-04-02 01:35:40 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -6,7 +6,10 @@ def square_array(array)
     expect(numbers).to_not receive(:inject)
     square_array(numbers)
   end
-  
+   it 'calls on each' do
+    numbers = [1,2,3]
+    expect(numbers).to receive(:each)
+    square_array(numbers)
   # your code here
 end
 
